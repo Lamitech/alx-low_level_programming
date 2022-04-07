60dab10be819fcb49df8d4e3a51f7e2fd8db6edb
Lamitech <folamiolamide95@gmail.com>
2022-04-07 14:38:20 -0700

    -0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - starting point of all c program.
+ * Return: return 0 when function is successful.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
