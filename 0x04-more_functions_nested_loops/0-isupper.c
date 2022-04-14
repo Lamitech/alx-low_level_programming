db3ee511cce96e5ef6ce0ea1c2d8302a7a64ea0d
Lamitech <folamiolamide95@gmail.com>
2022-04-14 14:51:30 -0700

    c4

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - prints for capital letter
+ * @c: parameter function
+ * Return: return 0 / 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
