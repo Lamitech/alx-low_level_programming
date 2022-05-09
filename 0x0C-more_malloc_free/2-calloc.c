7bce6935400f12fc44ace20162584bbe0b0a200f
Lamitech <folamiolamide95@gmail.com>
2022-05-09 12:59:41 -0700

    tassk remain 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,3 +28,22 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 
 }
+
+/**
+ * _memset - sets memeory
+ * @s: pointer params
+ * @b: params
+ * @n: parms
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
