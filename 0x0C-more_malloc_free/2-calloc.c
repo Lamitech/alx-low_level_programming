ee1996618c3626ce473de5c70569aa43ccd113e8
Lamitech <folamiolamide95@gmail.com>
2022-05-09 12:50:12 -0700

    tassk remain 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _calloc - allocate arr
+ * @nmemb: param
+ * @size: param
+ * Return: void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	/*NB memeset was created on prev project/task*/
+	_memset(p, 0, (nmemb * size));
+	return (p);
+
+
+}
